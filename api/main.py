02b7e97e49ea82fe319792e5f288266ba8ef557b
JosueBD <62859587+JosueBD@users.noreply.github.com>
2025-03-29 21:27:07 -0400

    Agregar path para importar profesor_ritmo_motores en Render
    ✅ Commit directly to the main branch

--- api/main.py
+++ api/main.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from fastapi import FastAPI, UploadFile
 from profesor_ritmo_motores import motor_generador, motor_analizador, motor_comparador
 import tempfile
@@ -30,3 +34,4 @@ async def comparar(file_objetivo: UploadFile, file_usuario: UploadFile):
         tmp1.write(await file_objetivo.read())
         tmp2.write(await file_usuario.read())
         return motor_comparador.comparar_ritmos(tmp1.name, tmp2.name)
+
